935b32e373578d6ce23bb91ae5d58c1a1fa44733
apatagg <31703613+apatagg@users.noreply.github.com>
2020-05-21 12:44:07 +0200

    Aplicado patron estrategia

---
+++ app/src/main/java/es/imposoft/twins/strategies/DealTwoDecks.java
@@ -0,0 +1,93 @@
+package es.imposoft.twins.strategies;
+
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import es.imposoft.twins.card.Card;
+import es.imposoft.twins.card.CardDecorator;
+import es.imposoft.twins.card.ConcreteCard;
+import es.imposoft.twins.card.SpecialCardDecorator1;
+import es.imposoft.twins.card.SpecialCardDecorator2;
+import es.imposoft.twins.card.SpecialCardDecorator3;
+import es.imposoft.twins.components.DeckTheme;
+import es.imposoft.twins.components.GameMode;
+import es.imposoft.twins.gametypes.Game;
+
+public class DealTwoDecks implements Dealer {
+    ArrayList<Card> shuffled;
+    ArrayList<Integer> numbers, imagesNormal, imagesChallenges;
+    List<Integer> challengesWon;
+    ArrayList<Integer> newCards;
+    int random, position, cards;
+    //ESTE NUMERO VARIA EN FUNCION DEL NUMERO DE CARTAS EXISTENTES PARA CADA TIPO DE BARAJA
+    int MAX_CARD_DESIGNS = 12;
+
+    @Override
+    public void assignCardTheme(DeckTheme theme, ArrayList<Card> concreteCards, Game game, Button[] buttons, Context context) {
+        shuffled = new ArrayList<>();
+        imagesNormal = new ArrayList<>();
+        imagesChallenges = new ArrayList<>();
+        numbers = new ArrayList<>();
+        newCards = new ArrayList<>();
+
+        cards = game.getCardAmount();
+
+        int totalChallenges = 0;
+        if(challengesWon != null) totalChallenges = challengesWon.size() * 2;
+
+        //Asignamos las cartas normales aleatoriamente entre las existentes
+        numbers = randomList();
+
+        for (int i = 0; i < cards / 2; i++) {
+            if (i % 2 == 0) {
+                imagesNormal.add(context.getResources().getIdentifier(DeckTheme.EMOJI.toString().toLowerCase() + numbers.get(i), "drawable", context.getPackageName()));
+            } else {
+                imagesNormal.add(context.getResources().getIdentifier(DeckTheme.CARS.toString().toLowerCase() + numbers.get(i), "drawable", context.getPackageName()));
+            }
+        }
+        //Images es un array con una imagen de cada una de las que hay que asignar
+
+        numbers.clear();
+
+        Collections.shuffle(Arrays.asList(buttons));
+        //Creamos todas las cartas contando las de desafios
+        for(int i = 0; i < cards; i++){
+            concreteCards.add(new ConcreteCard(buttons[i], context));
+        }
+
+        //Ya tenemos todas las cartas creadas pero sin la imagen asignada
+
+        imagesNormal.addAll(imagesNormal);
+
+        for (Card cardToAsign: concreteCards) {
+            if(cardToAsign instanceof ConcreteCard) {
+                int actualImage = imagesNormal.remove(0);
+                cardToAsign.setFrontImage(BitmapFactory.decodeResource(context.getResources(), actualImage));
+                cardToAsign.setBackImage(BitmapFactory.decodeResource(context.getResources(), context.getResources().getIdentifier("background2", "drawable", context.getPackageName())));
+                cardToAsign.setFrontName(actualImage);
+            }
+        }
+    }
+
+    private ArrayList<Integer> randomList() {
+        ArrayList<Integer> aux = new ArrayList<>();
+        if (cards == 24) {
+            for (int i = 0; i < cards / 2; i++)
+                aux.add(i);
+        } else {
+            while (aux.size() <= cards / 2) {
+                random = (int) (Math.random() * MAX_CARD_DESIGNS);
+                if (!aux.contains(random)) {
+                    aux.add(random);
+                }
+            }
+        }
+        return aux;
+    }
+}
---
+++ app/src/main/java/es/imposoft/twins/strategies/Dealer.java
@@ -0,0 +1,14 @@
+package es.imposoft.twins.strategies;
+
+import android.content.Context;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+import es.imposoft.twins.card.Card;
+import es.imposoft.twins.components.DeckTheme;
+import es.imposoft.twins.gametypes.Game;
+
+public interface Dealer {
+    void assignCardTheme(DeckTheme theme, ArrayList<Card> concreteCards, Game game, Button[] buttons, Context context);
+}
