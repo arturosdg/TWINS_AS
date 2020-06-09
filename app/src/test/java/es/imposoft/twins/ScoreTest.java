22793704bce7cf27213e6fa439dc0aeca2ad0269
leo-babogachi <almohanovidick.noemi@gmail.com>
2020-06-09 21:38:58 +0200

    Update ScoreTest.java

--- app/src/test/java/es/imposoft/twins/ScoreTest.java
+++ app/src/test/java/es/imposoft/twins/ScoreTest.java
@@ -3,7 +3,6 @@ package es.imposoft.twins;
 import org.junit.Test;
 import es.imposoft.twins.card.*;
 import es.imposoft.twins.prototype.*;
-
 import static org.junit.Assert.*;
 
 public class ScoreTest {
@@ -32,10 +31,13 @@ public class ScoreTest {
         ConcreteCard concreteCard = new ConcreteCard();
         SpecialCardDecorator1 specialCardDecorator1 = new SpecialCardDecorator1(concreteCard);
         AbstractScore scoreStandard = new ScoreStandard();
-
+        AbstractScore scoreLevels = new ScoreLevels();
         //Assert
         assertEquals("Points are not calculated correctly",15,scoreStandard.updateScore(true, specialCardDecorator1));
         assertEquals("Points are not calculated correctly",10,scoreStandard.updateScore(false, specialCardDecorator1));
+
+        assertEquals("Points are not calculated correctly",20,scoreLevels.updateScore(true, specialCardDecorator1));
+        assertEquals("Points are not calculated correctly",8,scoreLevels.updateScore(false, specialCardDecorator1));
     }
 
     @Test
@@ -44,10 +46,14 @@ public class ScoreTest {
         ConcreteCard concreteCard = new ConcreteCard();
         SpecialCardDecorator2 specialCardDecorator2 = new SpecialCardDecorator2(concreteCard);
         AbstractScore scoreStandard = new ScoreStandard();
+        AbstractScore scoreLevels = new ScoreLevels();
 
         //Assert
         assertEquals("Points are not calculated correctly",21,scoreStandard.updateScore(true, specialCardDecorator2));
         assertEquals("Points are not calculated correctly",16,scoreStandard.updateScore(false, specialCardDecorator2));
+
+        assertEquals("Points are not calculated correctly",28,scoreLevels.updateScore(true, specialCardDecorator2));
+        assertEquals("Points are not calculated correctly",16,scoreLevels.updateScore(false, specialCardDecorator2));
     }
 
     @Test
@@ -56,10 +62,14 @@ public class ScoreTest {
         ConcreteCard concreteCard = new ConcreteCard();
         SpecialCardDecorator3 specialCardDecorator3 = new SpecialCardDecorator3(concreteCard);
         AbstractScore scoreStandard = new ScoreStandard();
+        AbstractScore scoreLevels = new ScoreLevels();
 
         //Assert
         assertEquals("Points are not calculated correctly",27,scoreStandard.updateScore(true, specialCardDecorator3));
         assertEquals("Points are not calculated correctly",22,scoreStandard.updateScore(false, specialCardDecorator3));
+
+        assertEquals("Points are not calculated correctly",36,scoreLevels.updateScore(true, specialCardDecorator3));
+        assertEquals("Points are not calculated correctly",24,scoreLevels.updateScore(false, specialCardDecorator3));
     }
 
 
